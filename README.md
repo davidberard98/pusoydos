ad591dca8f7b84f01a5ee5dfafb8db2825d268af
David Berard <davidberard98@gmail.com>
2015-08-19 21:01:35 -0700

    First commit: Basic display, card movement

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pusoy Dos Online
+
+This is designed to be a online multiplayer pusoy dos game.
+
+Card images from [here](http://code.google.com/p/vector-playing-cards/).
+
+
---
+++ display.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html>
+<head>
+  <title>Play Pusoy Dos Online</title>
+  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+  <script type="text/javascript">
+  function drag(ev, tid)
+  {
+    tid.remove();
+  }
+
+  function setDragSettings()
+  {
+    $(".cardholder img").each(function(index, value){
+      //alert($(value).attr("src"));
+      $(value).on("dragstart", function(ev){
+        //alert("dragstart");
+        $(value).animate({"width":"0px"}, 300, function(){$(value).remove()});
+        //$(value).css("margin-right", "-100px");
+      });
+    });
+  }
+  $(document).ready(function(){
+    setDragSettings();
+  });
+
+  /*$(document).mousemove(function(e){
+    $("#image").css({left:e.pageX, top:e.pageY});
+  }); */
+  </script>
+</head>
+<body>
+<img src="cards/halfs/8s.png" id="image" style="position:absolute;" />
+<div class="cardpilevert">
+  <div class="cardpile">
+    <img src="cards/halfs/1s.png" alt="ace of spades" class="handcard" />
+    <img src="cards/halfs/1h.png" alt="ace of hearts" class="handcard" />
+    <img src="cards/halfs/2d.png" alt="two of diamonds" class="handcard" />
+  </div>
+</div>
+
+<div class="foot">
+  <div class="deckprep">
+    Prepare your hand
+  </div>
+  <div class="cardholder">
+    <img src="cards/halfs/1s.png" alt="ace of spades" class="handcard" draggable="true" />
+    <img src="cards/halfs/1h.png" alt="ace of hearts" class="handcard" />
+    <img src="cards/halfs/2d.png" alt="two of diamonds" class="handcard" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
+  </div>
+</div>
+</body>
+</html>
