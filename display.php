bb16728548bdddd82aec576d80b108f05c3dc4ab
David Berard <davidberard98@gmail.com>
2015-08-21 09:23:39 -0700

    Move to and from prep area, but it's pretty buggy

--- display.php
+++ display.php
@@ -3,69 +3,10 @@
 <head>
   <title>Play Pusoy Dos Online</title>
   <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <script src="pusoy.js"></script>
   <link rel="stylesheet" type="text/css" href="style.css" />
-  <script type="text/javascript">
-  function drag(ev, tid)
-  {
-    tid.remove();
-  }
-  var insidePrepArea = false;
-  var mouseX = -1;
-  var mouseY = -1; /*
-  $(document).on("dragover", function(event) {
-    mouseX = event.pageX;
-    mouseY = event.pageY;
-    console.log(mouseX + " " + mouseY)
-  }); */
-  $(document).on('dragover', function(evt) {
-    mouseX = evt.originalEvent.pageX,
-    mouseY= evt.originalEvent.pageY;
-});
-  var draggedCard = "none";
-  function setDragSettings()
-  {
-    $(".cardholder img").each(function(index, value){
-      //alert($(value).attr("src"));
-      $(value).on("dragstart", function(ev){
-        //alert("dragstart");
-        $(value).animate({"width":"70px"}, 300, function(){$(value).css("width", "0px")});
-        draggedCard = $(value).attr("src");
-        //$(value).css("margin-right", "-100px");
-      });
-      $(value).on('dragend', function(ev){
-        ev.preventDefault();
-        ev.stopPropagation();
-        var doff = $(".deckprep").offset();
-        var dw = $(".deckprep").width();
-        var dh = $(".deckprep").height();
-        //console.log("hi");
-        if(doff.left < mouseX && doff.left + dw > mouseX &&
-        doff.top < mouseY && doff.top + dh > mouseY)
-        {
-          console.log("Dropped inside box!");
-          $(".deckprep #words").text("");
-          $(".deckprep #cards").append("<img src='" + $(value).attr("src") + "' class='handcard' />");
-          $(this).remove();
-        }
-        else {
-          $(value).animate({"width":"100px"}, 300, function(){});
-        }
-      });
-    });
-  }
-  cactusWidth = $(this).width();
-  cactusHeight = $(this).height()-50;
-  $(document).ready(function(){
-    setDragSettings();
-  });
-
-  /*$(document).mousemove(function(e){
-    $("#image").css({left:e.pageX, top:e.pageY});
-  }); */
-  </script>
 </head>
 <body>
-<img src="cards/halfs/8s.png" id="image" style="position:absolute;" />
 <div class="cardpilevert">
   <div class="cardpile">
     <img src="cards/halfs/1s.png" alt="ace of spades" class="handcard" />
@@ -80,19 +21,19 @@
     <span id="cards"></span>
   </div>
   <div class="cardholder">
-    <img src="cards/halfs/1s.png" alt="ace of spades" class="handcard" draggable="true" />
-    <img src="cards/halfs/1h.png" alt="ace of hearts" class="handcard" />
-    <img src="cards/halfs/2d.png" alt="two of diamonds" class="handcard" />
-    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
-    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
-    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
-    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
-    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
-    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
-    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
-    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
-    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
-    <img src="cards/halfs/3c.png" alt="three of clubs" class="handcard" />
+    <img src="cards/halfs/1s.png" alt="ace of spades" data-cn="1s" class="handcard" draggable="true" />
+    <img src="cards/halfs/1h.png" alt="ace of hearts" data-cn="1h" class="handcard" draggable="true" />
+    <img src="cards/halfs/2d.png" alt="two of diamonds" data-cn="2d" class="handcard" draggable="true"/>
+    <img src="cards/halfs/3c.png" alt="three of clubs" data-cn="3c" class="handcard" draggable="true" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" data-cn="3c" class="handcard" draggable="true" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" data-cn="3c" class="handcard" draggable="true" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" data-cn="3c" class="handcard" draggable="true" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" data-cn="3c" class="handcard" draggable="true" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" data-cn="3c" class="handcard" draggable="true" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" data-cn="3c" class="handcard" draggable="true" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" data-cn="3c" class="handcard" draggable="true" />
+    <img src="cards/halfs/3c.png" alt="three of clubs" data-cn="3c" class="handcard" draggable="true" />
+    <img src="cards/halfs/8s.png" alt="eight of spades" data-cn="8s" class="handcard" draggable="true" />
   </div>
 </div>
 </body>
---
+++ pusoy.js
@@ -0,0 +1,89 @@
+function parseCN(cnval)
+{
+  var rval = cnval.substr(0, cnval.length-1);
+  rval = parseInt(rval);
+  var sval = cnval.substr(cnval.length-1, 1);
+  var output = {rank: rval, suite: sval};
+  return output;
+}
+
+var insidePrepArea = false;
+var mouseX = -1;
+var mouseY = -1; /*
+$(document).on("dragover", function(event) {
+  mouseX = event.pageX;
+  mouseY = event.pageY;
+  console.log(mouseX + " " + mouseY)
+}); */
+$(document).on('dragover', function(evt) {
+  mouseX = evt.originalEvent.pageX,
+  mouseY= evt.originalEvent.pageY;
+});
+var draggedCard = "none";
+function dragSetCardholder()
+{
+  $(".cardholder img").each(function(index, value){
+    //alert($(value).attr("src"));
+    $(value).on("dragstart", function(ev){
+      //alert("dragstart");
+      $(value).animate({"width":"70px"}, 300, function(){$(value).css("width", "0px")});
+      draggedCard = $(value).attr("src");
+      //$(value).css("margin-right", "-100px");
+    });
+    $(value).on('dragend', function(ev){
+      ev.preventDefault();
+      ev.stopPropagation();
+      var doff = $(".deckprep").offset();
+      var dw = $(".deckprep").width();
+      var dh = $(".deckprep").height();
+      //console.log("hi");
+      if(doff.left < mouseX && doff.left + dw > mouseX &&
+      doff.top < mouseY && doff.top + dh > mouseY)
+      {
+        console.log("Dropped inside box!");
+        $(".deckprep #words").text("");
+        $(".deckprep #cards").append("<img src='" + $(value).attr("src") + "' class='handcard' data-cn='" + $(value).attr("data-cn") + "' draggable='true' />");
+        $(this).remove();
+        dragSetDeckprep();
+      }
+      else {
+        $(value).animate({"width":"100px"}, 300, function(){});
+      }
+    });
+  });
+}
+function dragSetDeckprep()
+{
+  $(".deckprep #cards img").each(function(index, value){
+    $(this).on('dragstart', function(){
+      $(value).animate({"width":"70px"}, 300, function(){$(value).css("width", "0px")});
+      draggedCard = $(value).attr("src");
+    });
+    $(this).on('dragend', function(ev){
+      console.log("dsd end");
+      ev.preventDefault();
+      ev.stopPropagation();
+      var coff = $(".cardholder").offset();
+      var cw = $(".cardholder").width();
+      var ch = $(".cardholder").height();
+      //console.log("hi");
+      if(coff.left < mouseX && coff.left + cw > mouseX &&
+      coff.top < mouseY && coff.top + ch > mouseY)
+      {
+        console.log("Dropped back inside");
+        $(".cardholder").append("<img src='" + $(value).attr("src") + "' class='handcard' data-cn='" + $(value).attr("data-cn") + "' draggable='true' />");
+        $(this).remove();
+        dragSetCardholder();
+      }
+      else {
+        $(value).animate({"width":"100px"}, 300, function(){});
+      }
+    });
+  });
+}
+cactusWidth = $(this).width();
+cactusHeight = $(this).height()-50;
+$(document).ready(function(){
+  dragSetCardholder();
+  dragSetDeckprep();
+});
